ef14316b09d3fff131aa3f8872d3598f7c445f6f
Peechanok <42942851+Peechanok@users.noreply.github.com>
2020-11-14 07:25:06 +0700

    อัพเดท ui
    อัพเดท

---
+++ UI/navigation/MainTabScreen.js
@@ -0,0 +1,173 @@
+import React from 'react';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AntDesign, Ionicons, Entypo } from "@expo/vector-icons";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatScreen';
+import MyCoureScreen from '../screens/MyCoureScreen';
+import RoomScreen from '../screens/RoomScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#fff"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'หน้าหลัก',
+          tabBarColor: '#6495ED',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="mycourse"
+        component={MyCoureScreenStackScreen}
+        options={{
+          tabBarLabel: 'ห้องเรียนของฉัน',
+          tabBarColor: '#1f65ff',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="google-classroom" color={color} size={26} />
+          ),
+        }}
+      />
+    
+      <Tab.Screen
+        name="attend"
+        component={RoomScreenStackScreen}
+        options={{
+          tabBarLabel: 'เช็กรายชื่อ',
+          tabBarColor: '#694fad',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="checkbox-marked" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="message"
+        component={ChatScreenStackScreen}
+        options={{
+          tabBarLabel: 'ข้อความ',
+          tabBarColor: '#d02860',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="message-text-outline" color={color} size={26} />
+          ),
+        }}
+      />
+        <Tab.Screen
+        name="profile"
+        component={ProfileScreenStackScreen}
+        options={{
+          tabBarLabel: 'ฉัน',
+          tabBarColor: '#1f65ff',
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="user" size={26} color="#fff" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+    
+);
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => (
+<HomeStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#6495ED',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{
+        title:'Xlearning',
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#6495ED" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</HomeStack.Navigator>
+);
+
+const ProfileScreenStackScreen = ({navigation}) => (
+  <DetailsStack.Navigator screenOptions={{
+          headerStyle: {
+          backgroundColor: '#1f65ff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+          fontWeight: 'bold'
+          }
+      }}>
+          <DetailsStack.Screen name="ฉัน" component={ProfileScreen} options={{
+          headerLeft: () => (
+              <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
+          )
+          }} />
+  </DetailsStack.Navigator>
+  );
+const MyCoureScreenStackScreen = ({navigation}) => (
+<DetailsStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#1f65ff',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <DetailsStack.Screen name="ห้องเรียนของฉัน" component={MyCoureScreen} options={{
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</DetailsStack.Navigator>
+);
+const RoomScreenStackScreen = ({navigation}) => (
+  <DetailsStack.Navigator screenOptions={{
+          headerStyle: {
+          backgroundColor: '#1f65ff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+          fontWeight: 'bold'
+          }
+      }}>
+          <DetailsStack.Screen name="เช็กรายชื่อ" component={RoomScreen} options={{
+          headerLeft: () => (
+              <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
+          )
+          }} />
+  </DetailsStack.Navigator>
+  );
+  const ChatScreenStackScreen = ({navigation}) => (
+    <DetailsStack.Navigator screenOptions={{
+            headerStyle: {
+            backgroundColor: '#1f65ff',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+            fontWeight: 'bold'
+            }
+        }}>
+            <DetailsStack.Screen name="ข้อความ" component={ChatScreen} options={{
+            headerLeft: () => (
+                <Icon.Button name="ios-menu" size={25} backgroundColor="#1f65ff" onPress={() => navigation.openDrawer()}></Icon.Button>
+            )
+            }} />
+    </DetailsStack.Navigator>
+    );
+  
